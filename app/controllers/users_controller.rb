24a94f91a9795b0717271d47e8f30322a4f4c066
Paola <paola.carrai@tiscali.it>
2021-03-11 00:00:45 +0100

    aggiunta edit page del profilo e terminata parzialmente

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # handle a successfull update
+    else
+      render 'edit'
+    end
+  end
 
   private
 
